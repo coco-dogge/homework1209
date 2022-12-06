45282cab4c5feb270943a9fd2bb00420b8f19184
coco-dogge <brian123483@yahoo.com.tw>
2022-12-07 01:11:22 +0800

    first

---
+++ src/P10/P10A/main.java
@@ -0,0 +1,9 @@
+package P10.P10A;
+
+
+public class main {
+    public static void main(String[] args) {
+        P10.P10B.Car car1 = new P10.P10B.Car();
+        car1.show();
+    }
+}
---
+++ src/P6/main.java
@@ -0,0 +1,9 @@
+package P6;
+
+public class main {
+    public static void main(String[] args) {
+        Car car1;
+        car1 = new Car();
+        car1.show();
+    }
+}
